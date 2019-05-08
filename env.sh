e1bff9c1d3c4d962683460395e318fb3add493d5
musembi <musembinzioki@gmail.com>
2019-05-08 18:49:13 +0300

    Initiate gitignore and requirements

---
+++ env.sh
@@ -0,0 +1,21 @@
+export DATABASE_URL='postgres://msikilizaji:kifunguo@localhost:5432/skika_exp'
+
+# Proper envs
+export DJANGO_SECRET_KEY='4u(h3@twz#r(snmhvwtq5+i2z#d(df8$1el8ktkdh8z5h%=xem'
+export COOKIEKATA_DEBUG=true
+export SKIKA_DEFAULT_FROM_EMAIL='skika_dev@skika.com'
+# export SKIKA_API_HOST=''
+
+export FACEBOOK_APP_ID='592569741212328'
+export FACEBOOK_APP_SECRET='520a6cf891a88c3c20974a293e7334d1'
+
+export GOOGLE_CLIENT_ID='439447722070-cjnnhd95q7vsfj5ouc8t8oe0cehf00oq.apps.googleusercontent.com'
+export GOOGLE_CLIENT_SECRET=''eblzZFxasRbEddswW9DuWk4U''
+
+export SKIKA_DB_NAME='skika_exp'
+export SKIKA_DB_USER='msikilizaji'
+export SKIKA_DB_PASSWORD='kifunguoo'
+export SKIKA_DB_HOST='localhost'
+export SKIKA_DB_NAME='5432'
+
+export SKIKA_DATABASE_URL='postgres://msikilizaji:kifunguoo@localhost:5432/skika_exp'
