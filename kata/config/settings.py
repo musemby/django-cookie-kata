24b66fa1d80e6d1e72f8822d0103d02d351b2808
musembi <musembinzioki@gmail.com>
2019-05-11 14:24:35 +0300

    Set default permission class as IsAuthenticated

--- kata/config/settings.py
+++ kata/config/settings.py
@@ -54,6 +54,12 @@ INSTALLED_APPS = [
     'kata.kata_auth',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
